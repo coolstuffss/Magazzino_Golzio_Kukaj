4eca983039a764695759eae10e038372e217c0e1
SteveSevetS <StefanoGolzio@outlook.it>
2019-03-21 15:10:09 +0100

    initial committ

---
+++ Liste_XML/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <list>
+
+using namespace std;
+
+typedef struct prodotto
+{
+    int codice;
+    string nome;
+    int quantita;
+    float prezzo;
+}prodotto;
+
+int main()
+{
+    // list<int> sas;
+
+    // for(int i = 0; i < 10; i++)
+    // {
+    //     sas.push_back(i);
+    // }
+    
+    // for(auto i = sas.begin(); i != sas.end(); i++)
+    // {
+    //     cout << *i << endl;
+    // }
+    
+
+    system("pause");
+    return 0;
+}
